9687168cd175c4591aa5a793966a3145c55528c8
ArcticHonour <122731187+Evixityz@users.noreply.github.com>
2024-09-30 00:33:40 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -7,6 +7,13 @@ import platform
 from threading import Thread, Event
 
 stop_event = Event()
+theme = "lightblue"
+clr = theme
+
+if platform.system() == 'Linux':
+    os.system("clear")
+else:
+    os.system("cls")
 
 def send_http_requests(protocol, host, port, duration):
     timeout = time.time() + duration
@@ -25,6 +32,7 @@ def send_http_requests(protocol, host, port, duration):
             error_msg = f"Error sending request: {e}"
             print(error_msg)
             status_var.set(error_msg)
+            
 def save_input():
     protocol = protocol_var.get()
     host = host_entry.get().strip() 
@@ -55,13 +63,47 @@ def save_input():
         os.system("cls")
 
 def stop_requests():
-    stop_event.set()
+    theme_var.set()
     status_var.set("Requests stopped.")
+
+def change_theme():
+    global theme
+    font_settings = ("Arial")
+    text_color = "black"
+    if theme == 'lightblue':
+        theme = "#00ff07"
+    elif theme == "#00ff07":
+        theme = "lightyellow"
+    elif theme == "lightyellow":
+        theme = "pink"
+    elif theme == "pink":
+        theme = "#00a5ff"
+    elif theme == "#00a5ff":
+        theme = "white"
+    elif theme == "white":
+        theme = "grey"
+    elif theme == "grey":
+        theme = "#6814eb"
+    elif theme == "#6814eb":
+        theme = "black"
+        text_color = "lime"
+    elif theme == "black":
+        theme = "lightblue"
+        
+    root.config(bg=theme)
+    w.config(bg=theme , fg=text_color )
+    status_label.config(bg=theme, font=font_settings, fg=text_color)
+    protocol_label.config(bg=theme, font=font_settings, fg=text_color)
+    http_radio.config(bg=theme, font=font_settings, fg=text_color)
+    https_radio.config(bg=theme, font=font_settings, fg=text_color)
+    host_label.config(bg=theme, font=font_settings, fg=text_color)
+    port_label.config(bg=theme, font=font_settings, fg=text_color)
+    duration_label.config(bg=theme, font=font_settings, fg=text_color)
+
+
 
 root = tk.Tk()
 root.geometry("500x500")
-theme = "lightblue"
-clr = theme
 root.configure(bg=clr)
 root.title("Attax")
 
@@ -75,7 +117,7 @@ status_var = tk.StringVar(value="waiting for input...")
 status_entry = tk.Entry(root, textvariable=status_var, state='readonly', width=30, bg=theme)
 status_entry.place(x=150, y=160)
 
-protocol_var = StringVar(value='http')  # Default protocol
+protocol_var = StringVar(value='http')
 protocol_label = Label(root, text="Protocol:", bg=clr)
 protocol_label.place(x=50, y=200)
 http_radio = Radiobutton(root, text="HTTP", variable=protocol_var, value='http', bg=clr)
@@ -102,7 +144,11 @@ duration_scale.place(x=150, y=320)
 save_button = tk.Button(root, text="Launch attack", command=save_input)
 save_button.place(x=100, y=420)
 
+stop_button = tk.Button(root, text="Change theme", command=change_theme)
+stop_button.place(x=220, y=420)
+
 stop_button = tk.Button(root, text="Stop attack", command=stop_requests)
 stop_button.place(x=350, y=420)
+
 
 root.mainloop()
